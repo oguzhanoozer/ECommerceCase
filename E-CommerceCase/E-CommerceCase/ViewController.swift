9e73df4406b2e1552fdf6b08785145282b954c4a
oguzhanoozer <oguzoozer@gmail.com>
2025-02-05 13:22:06 +0300

    Initial project files added.

---
+++ E-CommerceCase/E-CommerceCase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  E-CommerceCase
+//
+//  Created by oguzhan on 5.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
